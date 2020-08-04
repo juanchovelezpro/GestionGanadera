15902b887303a376ca0dbf903a9ca3c5f6010f92
juanchovelezpro <velezolaya2012@hotmail.com>
2020-08-04 11:47:23 -0500

    Se actualiza la base de datos.

--- GestionGanadera/src/db/SQLConnection.java
+++ GestionGanadera/src/db/SQLConnection.java
@@ -47,17 +47,17 @@ public class SQLConnection {
 	public void createDatabase() {
 
 		try {
-			statement.executeUpdate("CREATE TABLE \"usuarios\" ( \"nombre\" TEXT, \"password\" TEXT, \"ubicacion\" TEXT, \"nombreFinca\" TEXT )");
-			statement.executeUpdate("CREATE TABLE \"vacunas\" ( \"id\" INTEGER, \"nombre\" TEXT NOT NULL, PRIMARY KEY(\"id\" AUTOINCREMENT) )");
-			statement.executeUpdate("CREATE TABLE \"potreros\" ( \"nombre\" TEXT NOT NULL UNIQUE, PRIMARY KEY(\"nombre\") )");		
+			statement.executeUpdate("CREATE TABLE \"potreros\" ( \"nombre\" TEXT NOT NULL UNIQUE, PRIMARY KEY(\"nombre\") )");
+			statement.executeUpdate("CREATE TABLE \"res\" ( \"numero\" TEXT NOT NULL UNIQUE, \"genero\" TEXT NOT NULL, \"color\" TEXT NOT NULL, \"fecha_nacimiento\" TEXT, \"observaciones\" TEXT, \"vivo\" INTEGER NOT NULL, \"embarazada\" INTEGER NOT NULL, \"fecha_embarazo\" TEXT,\r\n" + 
+					"\"fecha_ultima_purgado\" TEXT, \"fecha_ultima_vacunado\" TEXT, \"madreID\" TEXT, PRIMARY KEY(\"numero\") )");
 			statement.executeUpdate("CREATE TABLE \"purgantes\" ( \"id\" INTEGER, \"nombre\" TEXT NOT NULL, PRIMARY KEY(\"id\" AUTOINCREMENT) )");
-			statement.executeUpdate("CREATE TABLE \"res\" ( \"numero\" INTEGER NOT NULL UNIQUE, \"genero\" TEXT NOT NULL, \"color\" TEXT NOT NULL, \"fecha_nacimiento\" TEXT, \"observaciones\" TEXT, \"vivo\" INTEGER NOT NULL, \"embarazada\" INTEGER NOT NULL, \"fecha_embarazo\" INTEGER, \"fecha_ultima_purgado\" TEXT, \"fecha_ultima_vacunado\" TEXT, \"madreID\" INTEGER, PRIMARY KEY(\"numero\") )");
-			statement.executeUpdate("CREATE TABLE \"potreros_tiene_res\" ( \"id\" INTEGER UNIQUE, \"potreroNombre\" TEXT, \"resID\" INTEGER, PRIMARY KEY(\"id\" AUTOINCREMENT), FOREIGN KEY(\"resID\") REFERENCES \"res\"(\"numero\"), FOREIGN KEY(\"potreroNombre\") REFERENCES \"potreros\"(\"nombre\") )");
-			statement.executeUpdate("CREATE TABLE \"res_tiene_pesos\" ( \"id\" INTEGER, \"resID\" INTEGER NOT NULL, \"peso\" REAL NOT NULL, \"fecha\" TEXT NOT NULL, PRIMARY KEY(\"id\" AUTOINCREMENT), FOREIGN KEY(\"resID\") REFERENCES \"res\"(\"numero\") )");
-			statement.executeUpdate("CREATE TABLE \"res_tiene_vacunas\" ( \"id\" INTEGER, \"resID\" INTEGER, \"vacunaID\" INTEGER, \"fecha\" TEXT NOT NULL, PRIMARY KEY(\"id\" AUTOINCREMENT), FOREIGN KEY(\"vacunaID\") REFERENCES \"vacunas\"(\"id\"), FOREIGN KEY(\"resID\") REFERENCES \"res\"(\"numero\") )");
-			statement.executeUpdate("CREATE TABLE \"res_tiene_purgantes\" ( \"id\" INTEGER, \"resID\" INTEGER, \"purganteID\" INTEGER, PRIMARY KEY(\"id\" AUTOINCREMENT), FOREIGN KEY(\"resID\") REFERENCES \"res\"(\"numero\"), FOREIGN KEY(\"purganteID\") REFERENCES \"purgante\"(\"id\") )");
-			statement.executeUpdate("CREATE TABLE \"res_tiene_crias\" ( \"id\" INTEGER, \"madreID\" INTEGER, \"criaID\" INTEGER, PRIMARY KEY(\"id\" AUTOINCREMENT), FOREIGN KEY(\"madreID\") REFERENCES \"res\"(\"numero\"), FOREIGN KEY(\"criaID\") REFERENCES \"res\"(\"numero\") )");
-			
+			statement.executeUpdate("CREATE TABLE \"vacunas\" ( \"id\" INTEGER, \"nombre\" TEXT NOT NULL, PRIMARY KEY(\"id\" AUTOINCREMENT) )");
+			statement.executeUpdate("CREATE TABLE \"usuarios\" ( \"nombre\" TEXT, \"password\" TEXT, \"ubicacion\" TEXT, \"nombreFinca\" TEXT )");
+			statement.executeUpdate("CREATE TABLE \"potreros_tiene_res\" ( \"id\" INTEGER UNIQUE, \"potreroNombre\" TEXT, \"resID\" TEXT, FOREIGN KEY(\"potreroNombre\") REFERENCES \"potreros\"(\"nombre\"), FOREIGN KEY(\"resID\") REFERENCES \"res\"(\"numero\"), PRIMARY KEY(\"id\" AUTOINCREMENT) )");
+			statement.executeUpdate("CREATE TABLE \"res_tiene_crias\" ( \"id\" INTEGER, \"madreID\" TEXT, \"criaID\" TEXT, PRIMARY KEY(\"id\" AUTOINCREMENT), FOREIGN KEY(\"madreID\") REFERENCES \"res\"(\"numero\"), FOREIGN KEY(\"criaID\") REFERENCES \"res\"(\"numero\") )");
+			statement.executeUpdate("CREATE TABLE \"res_tiene_pesos\" ( \"id\" INTEGER, \"resID\" TEXT NOT NULL, \"peso\" REAL NOT NULL, \"fecha\" TEXT NOT NULL, PRIMARY KEY(\"id\" AUTOINCREMENT), FOREIGN KEY(\"resID\") REFERENCES \"res\"(\"numero\") )");
+			statement.executeUpdate("CREATE TABLE \"res_tiene_purgantes\" ( \"id\" INTEGER, \"resID\" TEXT, \"purganteID\" INTEGER, FOREIGN KEY(\"resID\") REFERENCES \"res\"(\"numero\"), FOREIGN KEY(\"purganteID\") REFERENCES \"purgante\"(\"id\"), PRIMARY KEY(\"id\" AUTOINCREMENT) )");
+			statement.executeUpdate("CREATE TABLE \"res_tiene_vacunas\" ( \"id\" INTEGER, \"resID\" TEXT, \"vacunaID\" INTEGER, \"fecha\" TEXT NOT NULL, PRIMARY KEY(\"id\" AUTOINCREMENT), FOREIGN KEY(\"vacunaID\") REFERENCES \"vacunas\"(\"id\"), FOREIGN KEY(\"resID\") REFERENCES \"res\"(\"numero\") )");
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
