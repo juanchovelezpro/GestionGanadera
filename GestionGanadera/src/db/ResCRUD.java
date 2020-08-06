6a896c75fb36d145e4362410a5815c3ed2c09c87
juanchovelezpro <velezolaya2012@hotmail.com>
2020-08-05 23:51:11 -0500

    Se actualiza el CRUD.

--- GestionGanadera/src/db/ResCRUD.java
+++ GestionGanadera/src/db/ResCRUD.java
@@ -4,6 +4,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+import model.Peso;
 import model.Res;
 
 public class ResCRUD {
@@ -107,32 +108,33 @@ public class ResCRUD {
 
 	}
 
-	public static void insertCria(String madreID, String criaID) {
+	public static ArrayList<Res> selectCria(String madreID) {
 
 		SQLConnection sql = SQLConnection.getInstance();
+		ArrayList<Res> crias = new ArrayList<>();
 
 		try {
-			sql.getStatement().executeUpdate(
-					"INSERT INTO res_tiene_crias (madreID,criaID) VALUES ('" + madreID + "','" + criaID + "'");
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-
-	}
-
-	public static ArrayList<Integer> selectCria(String madreID) {
-
-		SQLConnection sql = SQLConnection.getInstance();
-		ArrayList<Integer> idCrias = new ArrayList<>();
-
-		try {
-			ResultSet result = sql.getStatement()
-					.executeQuery("SELECT criaID FROM res_tiene_crias WHERE madreID='" + madreID + "'");
+			ResultSet result = sql.getStatement().executeQuery("SELECT * FROM res WHERE madreID='" + madreID + "'");
 
 			while (result.next()) {
 
-				idCrias.add(result.getInt(2));
+				String resID = result.getString(1);
+				String tipo = result.getString(2);
+				String genero = result.getString(3);
+				String color = result.getString(4);
+				String fecha_nacimiento = result.getString(5);
+				String observaciones = result.getString(6);
+				int vivo = result.getInt(7);
+				int embarazada = result.getInt(8);
+				String fecha_embarazo = result.getString(9);
+				String fecha_ultimo_purgado = result.getString(10);
+				String fecha_ultimo_vacunado = result.getString(11);
+				String madre_ID = result.getString(12);
+				String potreroNombre = result.getString(13);
 
+				Res res = new Res(resID, genero, tipo, color, vivo, fecha_nacimiento, observaciones, embarazada,
+						fecha_embarazo, madre_ID, fecha_ultimo_purgado, fecha_ultimo_vacunado, potreroNombre);
+				crias.add(res);
 			}
 
 		} catch (SQLException e) {
@@ -140,24 +142,61 @@ public class ResCRUD {
 			e.printStackTrace();
 		}
 
-		return idCrias;
+		return crias;
 	}
 
-	public static void deleteCria(String criaID) {
+	public static void insertPeso(String resID, double peso, String fecha) {
 
 		SQLConnection sql = SQLConnection.getInstance();
 
 		try {
-			sql.getStatement().executeUpdate("DELETE FROM res_tiene_crias WHERE criaID='" + criaID + "'");
+			sql.getStatement().executeUpdate("INSERT INTO res_tiene_pesos (resID,peso,fecha) VALUES ('" + resID + "',"
+					+ peso + ",'" + fecha + "')");
 		} catch (SQLException e) {
 
 			e.printStackTrace();
 		}
 
 	}
+	
+	public static ArrayList<Peso> selectPesos(String resID){
+		
+		SQLConnection sql = SQLConnection.getInstance();
+		ArrayList<Peso> pesos = new ArrayList<Peso>();
+		
+		try {
+			ResultSet result = sql.getStatement().executeQuery("SELECT * FROM res_tiene_pesos WHERE resID='"+resID+"'");
+			
+			while(result.next()) {
+				
+				double peso = result.getDouble(3);
+				String fecha = result.getString(4);
+				
+				pesos.add(new Peso(peso, fecha));
+				
+			}
+			
+		} catch (SQLException e) {
 
-	public static void updateCria() {
-
+			e.printStackTrace();
+		}
+		
+		return pesos;
+		
 	}
+	
+	public static void updatePeso(String resID,String fechaVieja, Peso peso) {
+		
+		SQLConnection sql = SQLConnection.getInstance();
+		
+		try {
+			sql.getStatement().executeUpdate("UPDATE res_tiene_pesos SET peso="+peso.getPeso()+", fecha='"+peso.getFecha()+" WHERE resID='"+resID+"' AND fecha='"+fechaVieja+"'");
+		} catch (SQLException e) {
+		
+			e.printStackTrace();
+		}
+		
+	}
+	
 
 }
